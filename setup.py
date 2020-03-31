44d0de8715a161f0e5cb24b1d9149fee90e6c622
Didan Deng <33117903+wtomin@users.noreply.github.com>
2020-03-31 21:26:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='steerable_pyramid',
-    version='0.1',
+    version='0.2',
     author='Didan Deng',
-    author_email='tomrunia@gmail.com',
-    url='https://github.com/tomrunia/PyTorchSteerablePyramid',
+    author_email='dengdidanwtomin@gmail.com',
+    url='https://github.com/wtomin/SteerablePyramid_Torch_TF_Numpy',
     description='Complex Steerable Pyramids in PyTorch, Numpy and Tensorflow',
     long_description='Fast CPU/CUDA implementation of the Complex Steerable Pyramid in PyTorch, Numpy and Tensorflow.',
     license='MIT',
